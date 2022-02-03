d8c291d8bed4849c38f0f0fed7edd63b67c8c53e
ed1van <edivan411@gmail.com>
2022-02-03 16:03:15 -0300

    Atualização do Blog Pessoal

--- Blog_Pessoal/meuBlog/meuBlog/src/main/java/org/generation/meuBlog/model/UsuarioModel.java
+++ Blog_Pessoal/meuBlog/meuBlog/src/main/java/org/generation/meuBlog/model/UsuarioModel.java
@@ -15,6 +15,8 @@ import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @Entity
 @Table(name = "tb_usuario")
 public class UsuarioModel {
@@ -26,6 +28,7 @@ private long id;
 @NotBlank(message = "Esse campo não pode ficar vazio")
 private String nome;
 
+@Schema(example = "email@email.com.br")
 @NotBlank(message = "Esse campo não pode ficar vazio")
 @Email(message = "O atributo Usuario deve ser um email válido") // deve ser formato email: @gmail.com
 private String usuario;
